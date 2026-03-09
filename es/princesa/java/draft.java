a4a7601282cdf718509f83ecbd935b97a4959bae
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-03-09 22:35:52 +0000

    quebrando cabeça

---
+++ es/princesa/java/draft.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class draft {
+    ArrayList<int> fila = new ArrayList<>();
+    int referencial;
+    
+    public String toString(){
+        String str += "[ ";           //[ 1> 2 3 ]
+        
+        for(int i=0; i<fila.size();i++){
+            str += fila.get(i);
+        }
+    }
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String ns = scan.nextLine();
+        String es = scan.nextLine();
+        int n = Integer.parseInt(ns);
+        int e = Integer.parseInt(es);
+        referencial = e;
+        
+        for(int i=0; i<n; i++){
+            fila.add(i);
+        }
+
+
+        for(int i=0; i<n; i++){
+            if(fila.size()==1)
+                break;
+            
+            if(fila.get(i)==e){
+                if((e+1) == n-1){
+                    fila.remove(e+1);
+                    e = fila.get(0);
+                }
+
+                else if((e) == n-1){
+                    fila.remove(0);
+                    e = fila.get(1);
+                }
+                
+                else{
+                    fila.remove(e+1);
+                    e = fila.get(e+2);
+                }
+            }
+            referencial = e;
+
+        }
+
+    }
+}
+
