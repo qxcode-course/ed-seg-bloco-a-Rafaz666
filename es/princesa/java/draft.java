e3638dfcc2b77a32abeacfe897046d9d2a90c8cf
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-03-12 01:29:58 +0000

    melhorias, mas array circular ainda confuso

--- es/princesa/java/draft.java
+++ es/princesa/java/draft.java
@@ -1,64 +1,75 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class draft {
-    static int[] fila;
-    static int referencial;
-    
+    static Scanner scan = new Scanner (System.in);
+    static Integer referencial;
+    static Integer[] fila;
+
+    public static void org(){
+        for(Integer i=0; i<fila.length; i++){
+            if(fila[i] == null){
+                fila[i] = fila[i+1];
+                fila[i+1] = null;
+            }
+        }
+    }
+
+    public static void select(){
+        for(Integer i=0; i<fila.length; i++){
+            if(referencial.equals(fila[i])){
+                if(referencial == fila.length){
+                    referencial = fila[i+1];
+                    kill(fila[0]);
+                }
+                else if (referencial == fila.length-1){
+                    referencial = fila[0];
+                    kill(fila[i+1]);
+                }
+                else{
+                    referencial = fila[i+2];
+                    kill(fila[i+1]);
+                }
+            }
+        }
+    }
+
+    public static void kill(Integer index){
+        fila[index] = null;
+        org();
+    }
+
     public static String toStringDraft(){
         String str = "";
         str += "[ ";
-        
-        for(int i=0; i<fila.size();i++){
-            str += fila[i];
-            if(fila[i]==referencial){
-                str += ">";
-            }
-            
-            if(i!=fila.size()-1){
-                str += " ";
-            }
-            
-            else{
-                str += " ]";
-            }
+        for(Integer i=0; i<fila.length; i++){
+            str += fila[i] + " ";
         }
+        str += "]";
 
         return str;
     }
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        String ns[] = scan.nextLine().split(" ");
-        int n = Integer.parseInt(ns[0]);
-        int e = Integer.parseInt(ns[1]);
-        referencial = e;
+
+    public static void main(String[] args){
+        Integer qtd = scan.nextInt();
+        referencial = scan.nextInt();
+        fila = new Integer[qtd];
         
-        for(int i=0; i<n; i++){
+        for(Integer i=0; i<fila.length; i++){
             fila[i] = i+1;
         }
 
-        System.out.println(fila.toString());
-
-        while(fila.size()!=1){
-        
-            if(fila.get(e+1) == n-1){
-                fila.remove(e+1);
-                e = fila.get(0);
-            }
-
-            else if(fila.get(e) == n-1){
-                fila.remove(0);
-                e = fila.get(0);
-            }
-                
-            else{
-                fila.remove(e+1);
-                e = fila.get(e+1);
-            }
-            
-            referencial = e;
-            System.out.println(fila.toString());
+        while(fila.length!=1){
+            select();
+            System.out.println(toStringDraft());
         }
+
+
+        
+
     }
+
+
+
+
 }
 
