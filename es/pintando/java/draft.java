ef66ed3b20f267a80029c3f21a4986b36a32377f
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-03-12 00:09:43 +0000

    corriji, aprendendo java fora de POO

--- es/pintando/java/draft.java
+++ es/pintando/java/draft.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class draft {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        String[] str = scan.next().split(" ");
+        String[] str = (scan.nextLine() + " " + scan.nextLine() + " " + scan.nextLine()).split(" ");
         double lados[] = Arrays.stream(str).mapToDouble(s -> Double.parseDouble(s)).toArray();
-        double p = (lados[0] + lados[1] + lados[2]);
+        double p = (lados[0] + lados[1] + lados[2])/2;
         double area = Math.sqrt(p * (p-lados[0]) * (p-lados[1]) * (p-lados[2])); 
         
-        System.out.println(area);
+        System.out.println(String.format("%.2f", area));
     }
 }
+
 
