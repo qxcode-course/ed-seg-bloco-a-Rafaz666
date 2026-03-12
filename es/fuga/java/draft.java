03484614b93b04b82a0a3ff2760bf38972b845b7
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-03-12 15:13:50 +0000

    quase

--- es/fuga/java/draft.java
+++ es/fuga/java/draft.java
@@ -1,6 +1,47 @@
+import java.util.*;
+
 public class draft {
+    static Scanner scan = new Scanner(System.in);
+    static boolean terminou;
+    
     public static void main(String args[]) {
-        System.out.println("qxcode");
+        int h = scan.nextInt();
+        int p = scan.nextInt();
+        int f = scan.nextInt();
+        int d = scan.nextInt();
+
+        if(d == -1){
+            for(f=f; terminou == false; p++){
+                if(f > 15)
+                    f=0;
+                
+                if(f == h){
+                    System.out.println("S");
+                    terminou = true;
+                }
+                else if(f == p){
+                    System.out.println("N");
+                    terminou = true;
+                }
+            }
+        }
+
+        if(d == 1){
+            for(f=f; terminou == false; p--){
+                if(f < 0)
+                    f=15;
+                
+                if(f == h){
+                    System.out.println("S");
+                    terminou = true;
+                }
+                else if(f == p){
+                    System.out.println("N");
+                    terminou = true;
+                }
+            }
+        }
     }
+
 }
 
