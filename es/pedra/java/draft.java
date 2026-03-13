7aff65da3f9db1cf83c4fb9239e91db80de55101
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-03-13 22:24:25 +0000

    erro no teste 3 da questão pedra de Revisão

---
+++ es/pedra/java/draft.java
@@ -0,0 +1,42 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class draft {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String args[]) {
+        int vez = scan.nextInt();
+        int[] jog = new int[vez];
+        int a;
+        int b;
+        int menor = 0;
+        int ganhador = 0;
+
+
+        for(int i=0; i<vez; i++){
+            a = scan.nextInt();
+            b = scan.nextInt();
+            if(a>=10 && b>=10){
+                jog[i] = Math.abs(a-b);
+            }
+        }
+
+        if (Arrays.stream(jog).allMatch(n -> n==jog[0])) {
+            System.out.println("sem ganhador");
+            return;
+        }
+        
+        menor = Arrays.stream(jog).filter(n -> n!=0).min().getAsInt();
+
+        for(int i=0; i<vez; i++){
+            if(menor==jog[i]){
+                ganhador = i;
+            }
+        }
+
+        System.out.println(ganhador);
+
+    }
+}
+
+
