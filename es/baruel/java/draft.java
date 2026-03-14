81399d04556d67a5de82ab315db26c20836c670e
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-03-14 12:44:03 +0000

    falta a saida

--- es/baruel/java/draft.java
+++ es/baruel/java/draft.java
@@ -1,6 +1,35 @@
+import java.util.*;
 public class draft {
+    static Scanner scan =  new Scanner(System.in);
+    static Integer[] figu = new Integer[qtd];
+    static Integer album = scan.nextInt();
+    static Integer qtd = scan.nextInt();
+
+    public static void ordenar(){
+        Integer[] ord = new Integer[album];
+        Integer menor;
+
+        for(Integer i=0; i<album; i++){
+            menor = Arrays.stream(figu).min(Integer::compareTo).get();
+            ord[i] = menor;
+            
+            for(Integer j=0; j<qtd; j++){
+                if(menor.equals(figu[j])){
+                    figu[j] = null;
+                }
+            }
+        }
+
+    }
     public static void main(String args[]) {
-        System.out.println("qxcode");
+        
+        for(Integer i=0;i<qtd; i++){
+            figu[i] = scan.nextInt();
+        }
+
+
+
+    
     }
 }
 
