96d7b9a96c2cf03d08805be2b3b5efafa45d9440
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-03-16 19:40:08 +0000

    fazendo

---
+++ es/gomos/java/draft.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class draft {
+    static Scanner scan =  new Scanner(System.in);
+    static int[] eixoX;
+    static int[] eixoY;
+    static  int qtd;
+    static String s;
+
+    public static void move(String str){
+        for(int i=0; i<qtd; i++){
+            
+            eixoX[i] = eixoX 
+        }
+    }
+    public static void main(String args[]) {
+        qtd = scan.nextInt();
+        scan.nextLine();
+        s = scan.nextLine();
+        eixoX = new int[qtd];
+        eixoY = new int[qtd];
+        
+        for(int i=0; i<qtd; i++){
+            eixoX[i] = scan.nextInt();
+            eixoY[i] = scan.nextInt();
+        }
+
+        move(s);
+    }
+}
+
--- es/princesa/java/draft.java
+++ es/princesa/java/draft.java
@@ -1,71 +1,40 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class draft {
     static Scanner scan = new Scanner (System.in);
-    static Integer referencial;
     static Integer[] fila;
+    static Integer qtd;
+    static Integer e;
 
     public static void org(){
-        for(Integer i=0; i<fila.length; i++){
-            if(fila[i] == null){
-                fila[i] = fila[i+1];
-                fila[i+1] = null;
-            }
-        }
+        fila = Arrays.stream(fila).filter(x -> x != null).toArray(Integer[]::new);
     }
 
-    public static void select(){
-        for(Integer i=0; i<fila.length; i++){
-            if(referencial.equals(fila[i])){
-                if(referencial == fila.length){
-                    referencial = fila[i+1];
-                    kill(fila[0]);
-                }
-                else if (referencial == fila.length-1){
-                    referencial = fila[0];
-                    kill(fila[i+1]);
+    public static void kill(){
+        for(int i=0; i<fila.length;i++){
+            if(fila.length>1 && e.equals(fila[i])){
+                if(fila[i+1]<fila.length){
+                    fila[i+1] = null;
+                    org();
                 }
                 else{
-                    referencial = fila[i+2];
-                    kill(fila[i+1]);
+                    fila[0] = null;
+                    org();
                 }
+            }
+            else{
+                break;
             }
         }
     }
-
-    public static void kill(Integer index){
-        fila[index] = null;
-        org();
-    }
-
-    public static String toStringDraft(){
-        String str = "";
-        str += "[ ";
-        for(Integer i=0; i<fila.length; i++){
-            str += fila[i] + " ";
-        }
-        str += "]";
-
-        return str;
-    }
-
+    
     public static void main(String[] args){
-        Integer qtd = scan.nextInt();
-        referencial = scan.nextInt();
-        fila = new Integer[qtd];
-        
-        for(Integer i=0; i<fila.length; i++){
-            fila[i] = i+1;
-        }
-
-        while(fila.length!=1){
-            select();
-            System.out.println(toStringDraft());
-        }
-
+        qtd = scan.nextInt();
+        e = scan.nextInt();
+        fila  = IntStream.rangeClosed(1,qtd).boxed().toArray(Integer[]::new);
 
         
-
     }
 
 
