2bc2fe4701b88f2462c83df8dd1af750b3808841
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-03-17 19:31:30 +0000

    quase feito

--- es/princesa/java/draft.java
+++ es/princesa/java/draft.java
@@ -6,26 +6,54 @@ public class draft {
     static Integer[] fila;
     static Integer qtd;
     static Integer e;
+    static boolean terminou = false;
 
     public static void org(){
         fila = Arrays.stream(fila).filter(x -> x != null).toArray(Integer[]::new);
     }
 
+    public static Integer outEdge(Integer i){
+    
+        if(i+1<fila.length && i+2<fila.length){
+            return null;
+        }
+        else if(i+1<fila.length && i+2 >= fila.length){
+            return 2;
+        }
+        else if(i+1>=fila.length){
+            return 1;
+        }
+
+        return null;
+
+    }
+
     public static void kill(){
         for(int i=0; i<fila.length;i++){
-            if(fila.length>1 && e.equals(fila[i])){
-                if(fila[i+1]<fila.length){
-                    fila[i+1] = null;
-                    org();
-                }
-                else{
-                    fila[0] = null;
-                    org();
+            if(fila.length > 1){
+                if(e.equals(fila[i])){
+                    if(outEdge(i)==null){
+                        fila[i+1] = null;
+                        e = fila[i+2];
+                        org();
+                    }
+                    else if(outEdge(i).equals(2)){
+                        fila[i+1] = null;
+                        e = fila[0];
+                        org();
+                    }
+                    else{
+                        fila[0] = null;
+                        e = fila[1];
+                        org();
+                    }
                 }
             }
             else{
+                terminou = true;
                 break;
             }
+
         }
     }
     
@@ -33,6 +61,23 @@ public class draft {
         qtd = scan.nextInt();
         e = scan.nextInt();
         fila  = IntStream.rangeClosed(1,qtd).boxed().toArray(Integer[]::new);
+        
+        while(terminou==false){
+            String str = "[ ";
+            
+            for(int i=0; i<fila.length; i++){
+                if(i<fila.length && e.equals(fila[i]))
+                    str += fila[i] + "> ";
+                else if(i<fila.length)
+                    str += fila[i] + " ";
+            }
+            
+            str += "]";
+            System.out.println(str);
+            kill();
+
+
+        }
 
         
     }
