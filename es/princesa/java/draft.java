4c54cc30821f72c3def8b73d9742c46f0dead6ee
Rafaz666 <antonio.rafael.holanda@gmail.com>
2026-03-19 11:15:38 -0300

    deu certo a lógica na mã, falta arrumar draftString

--- es/princesa/java/draft.java
+++ es/princesa/java/draft.java
@@ -6,46 +6,86 @@ public class draft {
     static Integer[] fila;
     static Integer qtd;
     static Integer e;
-    static int limite = fila.length;
+    static boolean end = false;
+
+    public static Integer limit(){
+        return fila.length;
+    }
 
     public static void org(){
         fila = Arrays.stream(fila).filter(x -> x != null).toArray(Integer[]::new);
     }
 
     public static int edge(int i){
-        if(fila[i+1]<limite && fila[i+2]<limite){
+        if(i+1<limit() && i+2<limit()){
             return 0;
         }
-        else if(fila[i+1]>=limite){
+        else if(i+1>=limit()){
             return 1;
         }
-        else if(fila[i+1]<limite && fila[i+2]>=limite){
+        else if(i+1<limit() && i+2>=limit()){
             return 2;
         }
         return -1;
     }
 
+    public static void kill(){
+        for(int i=0; i<fila.length; i++){
+
+            if(fila.length<=1){
+                end = true;
+                break;
+            }
+            
+            else if(fila[i].equals(e)){
+                switch (edge(i)) {
+                    case 0 -> {
+                        fila[i+1] = null;
+                        e = fila[i+2];
+                        org();
+                        break;
+                    }
+                    case 1 -> {
+                        fila[0] = null;
+                        e = fila[1];
+                        org();
+                        break;
+                    }
+                    case 2 -> {
+                        fila[i+1] = null;
+                        e = fila[0];
+                        org();
+                        break;
+                    }
+                    default -> System.out.println("deu pau");
+                }
+            }
+        }
+    }
+
     
+    public static void draftString(){
+        String str = "[ ";
+        
+        for(int i=0; i<fila.length; i++){
+            if(fila[i].equals(e))
+                str += fila[i] + "> ";
+            else
+                str += fila[i] + " ";
+        }
+
+        str += "]";
+
+        System.out.println(str);
+    }
     public static void main(String[] args){
         qtd = scan.nextInt();
         e = scan.nextInt();
         fila  = IntStream.rangeClosed(1,qtd).boxed().toArray(Integer[]::new);
 
-        for(Integer i : fila)
-            System.out.print(i + " ");
-
-        for(int i=0; i<fila.length; i++){
-            if(fila[i].equals(e)){
-                if(edge(i)==0){
-                    Sy
-                }
-            }
+        while(end==false){
+            draftString();
+            kill();
         }
-        
-        org();
-
-        for(Integer i : fila)
-            System.out.print(i + " ");
-
     }
 }
