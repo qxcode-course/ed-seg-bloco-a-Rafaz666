5c4ae9fbffde6ca57cbe403ccf6a7f745405906f
Rafaz666 <antonio.rafael.holanda@gmail.com>
2026-03-22 11:17:42 -0300

    fazendo gomos

--- es/gomos/java/draft.java
+++ es/gomos/java/draft.java
@@ -8,15 +8,34 @@ public class draft {
     static String s;
 
     public static void move(String str){
-        for(int i=0; i<qtd; i++){
-            
-            eixoX[i] = eixoX 
+        switch (s) {
+            case "R" -> {
+                eixoX[0] = eixoX[0] + 1;
+                for(int i=1; i<qtd; i++)
+                    eixoX[i] = eixoX[i-1] - 1;
+            }
+            case "L"-> {
+                eixoX[0] = eixoX[0] - 1;
+                for(int i=1; i<qtd; i++)
+                    eixoX[i] = eixoX[i-1] + 1;
+            }
+            case "D"-> {
+                eixoY[0] = eixoY[0] + 1;
+                for(int i=1; i<qtd; i++)
+                    eixoY[i] = eixoY[i-1] - 1;
+            }
+            case "U" -> {
+                eixoY[0] = eixoY[0] - 1;
+                for(int i=1; i<qtd; i++)
+                    eixoY[i] = eixoY[i-1] + 1;
+            }
+            default -> System.out.println("erro");       
         }
+
     }
     public static void main(String args[]) {
         qtd = scan.nextInt();
-        scan.nextLine();
-        s = scan.nextLine();
+         s = scan.nextLine();
         eixoX = new int[qtd];
         eixoY = new int[qtd];
         
@@ -26,6 +45,12 @@ public class draft {
         }
 
         move(s);
+
+        String str = "";
+        for(int i=0; i<qtd; i++)
+            str += eixoX[i] + " " + eixoY[i] + "\n";
+        
+        System.out.println(str);
     }
 }
 
