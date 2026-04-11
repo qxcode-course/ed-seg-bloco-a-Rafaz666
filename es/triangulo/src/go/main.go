9e41e1e5a0c20ce2490e03bbd03f6087c8c2a4f5
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-04-11 18:56:09 +0000

    codespace: consertando

--- es/triangulo/src/go/main.go
+++ es/triangulo/src/go/main.go
@@ -7,13 +7,13 @@ import (
 	"strings"
 )
 
-func convert(vet []int) []string{
-	str := []string{}
-	for i:=0; i<len(vet); i++{
-		str[i], _ = strconv.Atoi(vet[i])
+func convert(vet []int) string {
+	str := ""
+	for i := 0; i < len(vet); i++ {
+		str += strconv.Itoa(vet[i]) + " "
 	}
 
-	return str
+	return "[ " + str + "]"
 }
 
 func processa(vet []int, i int, str []string) []string {
@@ -22,14 +22,16 @@ func processa(vet []int, i int, str []string) []string {
 		return str
 	}
 
-	newvet := soma(vet, -1, str)
+	emp := []int{}
+	newvet := soma(vet, -1, emp)
 	str[i] = convert(newvet)
+
 	return processa(newvet, i, str)
 }
 
 func soma(vet []int, i int, value []int) []int {
 	i++
-	if i == len(vet)-1 {
+	if i == len(vet)-1 || len(vet) == 0 {
 		return value
 	}
 
@@ -53,5 +55,5 @@ func main() {
 			vet = append(vet, value)
 		}
 	}
-	processa(vet, -1, "")
+	processa(vet, -1, []string{})
 }
