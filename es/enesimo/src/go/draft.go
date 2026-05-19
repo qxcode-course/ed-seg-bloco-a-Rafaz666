4bdc521306b8234de784ad9433d347e042b92eec
Rafael Sampaio <antonio.rafael.holanda@gmail.com>
2026-05-19 18:51:49 +0000

    enesimo feito

--- es/enesimo/src/go/draft.go
+++ es/enesimo/src/go/draft.go
@@ -4,22 +4,31 @@ import (
 	"fmt"
 )
 
-func enesimo(num, div int) int {
+func enesimo(pos, cont, num int) int {
+	if cont == pos {
+		return num
+	}
 
+	num++
+	if ehprimo(num, 2) == true {
+		cont++
+	}
+
+	return enesimo(pos, cont, num)
 }
 
-func ehprimo(x int, div int) (bool, int) {
+func ehprimo(x int, div int) bool {
 
 	if div == x {
-		return true, x
+		return true
 	}
 
 	if x < 2 {
-		return false, -1
+		return false
 	}
 
 	if x%div == 0 {
-		return false, -1
+		return false
 	}
 
 	return ehprimo(x, div+1)
@@ -27,6 +36,6 @@ func ehprimo(x int, div int) (bool, int) {
 
 func main() {
 	var num int
-	fmt.Scan("%d", &num)
-	fmt.Println(enesimo(num, 2))
+	fmt.Scan(&num)
+	fmt.Println(enesimo(num, 0, 1))
 }
