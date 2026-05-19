43e9effa346123d6d92634c3844e5e28ba4de540
Rafaz666 <antonio.rafael.holanda@gmail.com>
2026-05-19 12:47:51 -0300

    fazendo priminhos

--- es/priminhos/src/go/draft.go
+++ es/priminhos/src/go/draft.go
@@ -1,5 +1,46 @@
 package main
+
 import "fmt"
+
+func ehprimo(x int, div int) bool {
+
+	if div == x {
+		return true
+	}
+
+	if x < 2 {
+		return false
+	}
+
+	if x%div == 0 {
+		return false
+	}
+
+	return ehprimo(x, div+1)
+}
+
+func Gerated(primos int) []int {
+	vet := make([]int, primos)
+	qtd := 0
+	eh := false
+	for i := 2; qtd < primos; i++ {
+		for j := 2; eh == false; j++ {
+			if ehprimo(i, j) == true {
+				eh = true
+				vet[qtd] = i
+				qtd++
+				break
+			}
+		}
+
+		eh = false
+	}
+
+	return vet
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	var num int
+	fmt.Scan(&num)
+	fmt.Println(Gerated(num))
 }
