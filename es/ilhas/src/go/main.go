3dc4ff44270ab3c3063b083c5ed8badcde99f46d
Rafaz666 <antonio.rafael.holanda@gmail.com>
2026-06-01 12:17:54 -0300

    fazendo dfs 3

--- es/ilhas/src/go/main.go
+++ es/ilhas/src/go/main.go
@@ -6,26 +6,55 @@ import (
 	"os"
 )
 
-// Não modifique a assinatura da função numIslands
-// Ela é a função que será chamada no LeetCode para resolver o problema
-func numIslands(grid [][]byte) int {
-	//
-	_ := grid
-	return 0
+func inLimit(grid [][]rune, l, c int) bool {
+	if (l >= 0 && l < len(grid)) && (c >= 0 && c < len(grid[l]) && grid[l][c] == '1') {
+		return true
+	}
+
+	return false
 }
 
-// Não modifique a função main
+func dfs(grid [][]rune, l, c int) {
+	if inLimit(grid, l, c) {
+		grid[l][c] = '0'
+		dfs(grid, l+1, c)
+		dfs(grid, l-1, c)
+		dfs(grid, l, c+1)
+		dfs(grid, l+1, c-1)
+	} else {
+		return
+	}
+}
+
+// Não modifique a assinatura da função numIslands
+// Ela é a função que será chamada no LeetCode para resolver o problema
+
+func numIslands(grid [][]rune) int {
+	num := 0
+	for l := 0; l < len(grid); l++ {
+		for c := 0; c < len(grid[l]); c++ {
+			if grid[l][c] == '1' {
+				dfs(grid, l, c)
+				num++
+			}
+		}
+	}
+	return num
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	line := scanner.Text()
 	var nl, nc int
 	fmt.Sscanf(line, "%d %d", &nl, &nc)
-	grid := make([][]byte, nl)
+
+	grid := make([][]rune, nl)
 	for i := 0; i < nl; i++ {
 		scanner.Scan()
-		grid[i] = []byte(scanner.Text())
+		grid[i] = []rune(scanner.Text())
 	}
+
 	result := numIslands(grid)
 	fmt.Println(result)
 }
